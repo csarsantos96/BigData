5be60edfb203c10ae54a740e51e7ecbc4fce0475
Cesar Santos <csar.santos18@gmail.com>
2025-05-27 11:38:39 -0500

    first commit final work

---
+++ top_10_more_wins.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_theme(style="ticks")
+
+statsNFL = pd.read_csv('team_stats_2003_2023.csv')
+
+columnYear = 'year'
+columnRaNking = 'wins'
+columnTeam = 'team'
+
+total_wins = statsNFL.groupby(columnTeam)[columnRaNking].sum().sort_values(ascending=False).reset_index()
+print("\nTimes com mais vitórias entre 2003 e 2023: ")
+print(total_wins.head(10))
+
+plt.figure(figsize=(10, 6))
+sns.barplot(data=total_wins.head(10), x=columnTeam, y=columnRaNking, palette="Blues_d")
+plt.title("Top 10 Times com Mais Vitórias (2003–2023)")
+plt.ylabel("Total de Vitórias")
+plt.xlabel("Time")
+plt.xticks(rotation=45, ha='right')
+
+for bar in plt.gca().patches:
+    height = bar.get_height()
+    plt.text(bar.get_x() + bar.get_width() / 2, height + 1, f'{int(height)}',
+             ha='center', va='bottom', fontsize=10)
+
+plt.tight_layout()
+plt.savefig("top_10_vitorias.png")
+plt.show()
---
+++ turnoversXfirstDown.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set_theme(style="ticks")
+
+statsNFL = pd.read_csv('team_stats_2003_2023.csv')
+
+columnYear = 'year'
+columnTurnovers = 'turnovers'
+columnFirstDown = 'first_down'
+columnTeam = 'team'
+
+# --- Top 5 times com mais Turnovers ---
+top5_turnovers = statsNFL.groupby(columnTeam)[columnTurnovers].sum().sort_values(ascending=False).head(5).index.tolist()
+df_turnovers = statsNFL[statsNFL[columnTeam].isin(top5_turnovers)]
+
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=df_turnovers, x=columnYear, y=columnTurnovers, hue=columnTeam, palette="Set1")
+plt.title("Mais de Turnovers sofridos por Ano (Top 5 Times)")
+plt.xlabel("Ano")
+plt.ylabel("Total de Turnovers")
+plt.legend(title="Time", bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.tight_layout()
+plt.savefig("turnovers.png")
+plt.show()
+
+# --- Top 5 times com mais First Downs ---
+top5_firstdowns = statsNFL.groupby(columnTeam)[columnFirstDown].sum().sort_values(ascending=False).head(5).index.tolist()
+df_firstdowns = statsNFL[statsNFL[columnTeam].isin(top5_firstdowns)]
+
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=df_firstdowns, x=columnYear, y=columnFirstDown, hue=columnTeam, palette="Set2")
+plt.title("Mais  de First Downs por Ano (Top 5 Times)")
+plt.xlabel("Ano")
+plt.ylabel("Total de First Downs")
+plt.legend(title="Time", bbox_to_anchor=(1.05, 1), loc='upper left')
+plt.tight_layout()
+plt.savefig("firstDowns.png")
+plt.show()
